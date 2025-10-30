90f53f0fa7dbfd3522ee81e30dbc2c23e5159906
Kisaragi Hiu <mail@kisaragi-hiu.com>
2025-10-31 05:56:17 +0900

    We only need to patch evil-delete

--- evil-cutlass.el
+++ evil-cutlass.el
@@ -43,68 +43,38 @@
   :group 'evil-cutlass
   :type 'string)
 
-(defun evil-cutlass--third-advice (cmd &rest args)
-  "Use this as an advice like this:
+;; A manually collected stack of evil commands that end up calling evil-yank:
+;;
+;; (legend:
+;;  the star: these are ones we want to override
+;;  the colon: this function is called by something else, listed below
+;;  the period: this function is not directly called by anything else)
+;;
+;; evil-yank:
+;;   evil-yank-line.
+;;   evil-delete(*):
+;;     evil-change(*):
+;;       evil-substitute(*).
+;;       evil-change-line(*).
+;;       evil-change-whole-line(*):
+;;         evil-change-line(*).
+;;     evil-delete-line(*).
+;;     evil-delete-whole-line(*).
+;;     evil-delete-char(*).
+;;     evil-delete-backward-char(*).
+;;     evil-visual-paste:
+;;       evil-paste-before.
+;;       evil-paste-after.
+;;
+;; This means we can simply override evil-delete.
 
-  (advice-add \\='evil-change-whole-line
-              :around #\\='evil-cutlass--third-advice)
-
-This will then make `evil-change' use the black hole register by default.
-
-The \"third\" in the name refers to the fact that `evil-change'
-accepts REGISTER as the fourth argument. This has to be done as
-`evil-this-register' is read in the interactive declaration,
-which is run before advices, so setting it in an advice like this
-will not have any effect. For commands that accept REGISTER as
-the fourth argument, use `evil-cutlass--fourth-advice' instead.
-
-CMD is the original function. ARGS is the argument list."
-  ;; If `evil-this-register' is nil, that means the user did not
-  ;; specify another register, so we swap out the register argument
-  ;; with the black hole register. It isn't cleared until the end of
-  ;; the command, allowing us to still see its value at this point.
-  (unless evil-this-register
-    (setq args (list (elt args 0)
-                     (elt args 1)
-                     ?_
-                     ;; nthcdr = -drop
-                     (nthcdr 3 args))))
-  (apply cmd args))
-
-(defun evil-cutlass--fourth-advice (cmd &rest args)
-  "Use this as an advice like this:
-
-  (advice-add \\='evil-change
-              :around #\\='evil-cutlass--fourth-advice)
-
-This will then make `evil-change' use the black hole register by default.
-
-The \"fourth\" in the name refers to the fact that `evil-change'
-accepts REGISTER as the fourth argument. This has to be done as
-`evil-this-register' is read in the interactive declaration,
-which is run before advices, so setting it in an advice like this
-will not have any effect. For commands that accept REGISTER as
-the third argument, use `evil-cutlass--third-advice' instead.
-
-CMD is the original function. ARGS is the argument list."
-  ;; If `evil-this-register' is nil, that means the user did not
-  ;; specify another register, so we swap out the register argument
-  ;; with the black hole register. It isn't cleared until the end of
-  ;; the command, allowing us to still see its value at this point.
-  (unless evil-this-register
-    (setf (elt args 3) ?_))
-  (apply cmd args))
-
-(defvar evil-cutlass--commands
-  '((evil-change                 :advice evil-cutlass--fourth-advice)
-    (evil-change-line            :advice evil-cutlass--fourth-advice)
-    (evil-substitute             :advice evil-cutlass--fourth-advice)
-    (evil-change-whole-line      :advice evil-cutlass--third-advice)
-    (evil-org-delete             :advice evil-cutlass--fourth-advice)
-    (evil-delete                 :advice evil-cutlass--fourth-advice)
-    (evil-delete-line            :advice evil-cutlass--fourth-advice)
-    (evil-delete-char            :advice evil-cutlass--fourth-advice)
-    (evil-delete-backward-char   :advice evil-cutlass--fourth-advice)))
+(defun evil-cutlass--redirect-to-blackhole-advice (orig beg end &optional type register yank-handler)
+  "Advice to make `evil-delete' use the blackhole register by default.
+ORIG is the original function.
+BEG, END, TYPE, REGISTER, and YANK-HANDLER are `evil-delete' arguments."
+  (unless (or register (eq this-command 'evil-visual-paste))
+    (setq register ?_))
+  (funcall orig beg end type register yank-handler))
 
 ;;;###autoload
 (defalias 'evil-cutlass-cut (symbol-function 'evil-delete))
@@ -122,16 +92,9 @@ CMD is the original function. ARGS is the argument list."
       (progn
         ;; update any possible changes to `evil-delete'
         (fset 'evil-cutlass-cut (symbol-function 'evil-delete))
-        ;; add advice
-        (dolist (command-cfg evil-cutlass--commands)
-          (let ((cmd (car command-cfg))
-                (advice (plist-get (cdr command-cfg) :advice)))
-            (advice-add cmd :around advice))))
+        (advice-add #'evil-delete :around #'evil-cutlass--redirect-to-blackhole-advice))
     ;; remove advice
-    (dolist (command-cfg evil-cutlass--commands)
-      (let ((cmd (car command-cfg))
-            (advice (plist-get (cdr command-cfg) :advice)))
-        (advice-remove cmd advice)))))
+    (advice-remove #'evil-delete #'evil-cutlass--redirect-to-blackhole-advice)))
 
 (provide 'evil-cutlass)
 
